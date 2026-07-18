72a97abc61a39c99aaa10753c758dc0f9cd6045a
Muhammad Musa Ahmad <syedmusasm9@gmail.com>
2026-07-18 17:55:19 +0500

    18-7-26 Updated commit

--- PYTHON/Learning.py
+++ PYTHON/Learning.py
@@ -1,1 +1,36 @@
-print("Hello, SM")
+# print("Hello, SM")
+
+# a = input("Enter a number: ")
+# print(f"Multiplicationtable of {a} is: ")
+
+# try:
+#     for i in range(1, 11):
+#         print(f"{int(a)} x {i} = {int(a)*i}")
+# except: 
+#     print("Invlid input")
+
+
+# def func1():
+#     try: 
+#         list = [1,2,3,4,5]
+#         i = int(input("Enter the index: "))
+#         print(list[i])
+#         return 1
+#     except:
+#         print("Invalid input")
+#         return 0
+
+#     finally:
+#         print("you always know my name")
+
+# x = func1()
+# print(x)
+
+
+a = input("Enter quit: ")
+if(a != "quit"):
+    raise ValueError("Enter only quit")
+
+
+
+
